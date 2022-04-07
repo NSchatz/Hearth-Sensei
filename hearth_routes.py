f9041eb21eea2ca01050bd5917d85fd8418738ae
NSchatz <noah.lane.schatz@gmail.com>
2022-04-07 00:01:01 -0400

    Setup react battle page with dropdowns and fetch calls

--- hearth_routes.py
+++ hearth_routes.py
@@ -20,34 +20,60 @@ hearth_routes = flask.Blueprint(
     template_folder="./static/react",
 )
 
-
 @hearth_routes.route("/", methods=["POST", "GET"])
 # @login_required
 def index():
+#     response1 = requests.request("GET", url, headers=headers)
+#     re1 = response1.json()
+#     clean_data =  json.loads(response1)
+#     print(clean_data)
+#     for thing in clean_data:
+#         if clean_data[0]['img'] == None:
+#             clean_data[0].remove[0]
+    
+#     print(clean_data)
+    
+#     if flask.request.method == "POST":
+#         card = str(flask.request.form.get("card"))
+#         card2 = str(flask.request.form.get("card2"))
+#         print(card)
+#         url2 = "https://omgvamp-hearthstone-v1.p.rapidapi.com/cards/" + card
+#         response2 = requests.request("GET", url2, headers=headers)
+#         re2 = response2.json()
+#         url3 = "https://omgvamp-hearthstone-v1.p.rapidapi.com/cards/" + card2
+#         response3 = requests.request("GET", url3, headers=headers)
+#         re3 = response3.json()
+        
+#         print(re3)
+        
+#         return flask.render_template("battle.html", re1=re1, re2=re2, re3=re3)
+    
+     return flask.render_template("index.html")
+
+@hearth_routes.route("/getcards", methods=["GET", "POST"])
+def getcards():
     response1 = requests.request("GET", url, headers=headers)
     re1 = response1.json()
-    clean_data =  json.loads(response1)
-    print(clean_data)
-    for thing in clean_data:
-        if clean_data[0]['img'] == None:
-            clean_data[0].remove[0]
-    
-    print(clean_data)
-    
-    if flask.request.method == "POST":
-        card = str(flask.request.form.get("card"))
-        card2 = str(flask.request.form.get("card2"))
-        print(card)
-        url2 = "https://omgvamp-hearthstone-v1.p.rapidapi.com/cards/" + card
-        response2 = requests.request("GET", url2, headers=headers)
-        re2 = response2.json()
-        url3 = "https://omgvamp-hearthstone-v1.p.rapidapi.com/cards/" + card2
-        response3 = requests.request("GET", url3, headers=headers)
-        re3 = response3.json()
-        
-        print(re3)
-        
-        return flask.render_template("battle.html", re1=re1, re2=re2, re3=re3)
-    
-    return flask.render_template("battle.html", re1=re1)
+    filtered = [project for project in re1 if project.get('img') is not None]
+    print(filtered)
+    return flask.jsonify(filtered)
+
+# @hearth_routes.route("/getcardinfo", methods=["GET", "POST"])
+# def getcardinfo():
+#     revs = []
+#     user = current_user.username
+#     ratings = Ratings.query.filter_by(user=user).all()
+#     i = 0
+#     for things in ratings:
+#         revs.append(
+#             {
+#                 "index": i,
+#                 "id": things.id,
+#                 "movieid": things.movieid,
+#                 "rating": things.rating,
+#                 "review": things.comment,
+#             }
+#         )
+#         i += 1
+#     return jsonify(revs)
 
--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from "react";
+let cards = '/getcards'
+let getcardinfo = '/getcardinfo'
 
 function App() {
+  const [Cards, setCards] = useState([]);
+  const [Card1, setCard1] = useState([]);
+  const [Card2, setCard2] = useState([]);
+  useEffect(() => {
+    fetch(cards, {
+      method: 'POST',
+      headers: {'Content-Type': 'application/json'}
+    }
+    ).then(res => res.json()).then(data => setCards(data));
+  }, []);
+  console.log(Cards)
+
+  function onChange1(e){
+    const filtered = Cards.filter(entry => Object.values(entry).some(val => typeof val === "string" && val.includes(e.target.value)));
+    setCard1(filtered);
+    console.log(Card1);
+  }
+  function onChange2(e){
+    const filtered = Cards.filter(entry => Object.values(entry).some(val => typeof val === "string" && val.includes(e.target.value)));
+    setCard2(filtered);
+    console.log(Card2);
+  }
+  function onSubmit(event){
+    event.preventDefault();
+    console.log("refresh prevented");
+    var card1 = document.getElementById("Card1");
+    var card2 = document.getElementById("Card2");
+    console.log(Card1);
+    console.log(Card2.img);
+    console.log(Card2);
+    console.log(Card2.img);
+    return( 
+      <img src={Card1.img} />,
+      <img src={Card2.img} />
+    )
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form action="" onSubmit={onSubmit}>
+        <select name="1" id="" onChange={(e) => onChange1(e)}>
+          {Cards.map((item)=> <option id='card1' key={item} value={ item.cardId }>{ item.name }</option>)}
+        </select>
+        <select name="2" id="" onChange={(e) => onChange2(e)}>
+          {Cards.map((item)=> <option id='card2' key={item} value={ item.cardId }>{ item.name }</option>)}
+        </select>
+        <input type="Submit"/>
+      </form>
+      {Card1.map((object) =>  <div>Attack:{object.attack} Health:{object.health}<img src={object.img}/>,</div>)}
+      {Card2.map((object) => <div>Attack:{object.attack} Health:{object.health}<img src={object.img}/>,</div>)}
     </div>
   );
 }
