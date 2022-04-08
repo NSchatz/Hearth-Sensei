9c1617927b91d4a17860909bb80eba5aa26614cf
kevinalbino <kevinalbi4625@gamil.com>
2022-04-08 01:07:29 -0400

    Made certain values able to be non unique as it didnt allow for multiple posts from one user

--- models.py
+++ models.py
@@ -15,11 +15,11 @@ class Users(UserMixin, db.Model):
 
 class History(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(100), unique=True, nullable=False)
-    card1 = db.Column(db.String(100), unique=True, nullable=False)
+    username = db.Column(db.String(100), nullable=False)
+    card1 = db.Column(db.String(100), nullable=False)
     card1_attack = db.Column(db.Integer)
     card1_health = db.Column(db.Integer)
-    card2 = db.Column(db.String(100), unique=True, nullable=False)
+    card2 = db.Column(db.String(100), nullable=False)
     card2_attack = db.Column(db.Integer)
     card2_health = db.Column(db.Integer)
-    winner = db.Column(db.String(100), unique=True, nullable=False)
+    winner = db.Column(db.String(100), nullable=False)
