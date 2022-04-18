8a87b89d2f2116fa6b7c7bbe0b6cf3006d86ecba
Gerald Guerrero <sbgeraldg@gmail.com>
2022-04-17 20:56:41 -0400

    converted static profile page to react page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Battle from "./Pages/Battle"
 import Landing from "./Pages/Landing"
+import Profile from "./Pages/Profile"
 import Error from "./Pages/Error"
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
       <Routes>
         <Route exact path="/" element={<Landing />} />
         <Route path="/battle" element={<Battle />} />
+        <Route path="/Profile" element={<Profile />} />
         <Route path="*" element={<Error />} />
       </Routes>
     </Router>
