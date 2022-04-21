3591c382146e81d41b66542d2713e5fe2ada04dc
Gerald Guerrero <sbgeraldg@gmail.com>
2022-04-21 16:57:30 -0400

    combined portions of profile/user code into 1 component and rewrote some flask routes

--- src/Pages/Profile.js
+++ src/Pages/Profile.js
@@ -1,109 +1,25 @@
 import './Profile.css';
+import ProfileTemplate from './ProfileTemplate';
 import React, { useState, useEffect } from "react";
 
-function HistoryRow(props) {
-    var battle = props.value;
-    console.log(battle);
-    return (
-        <tr>
-            <td>
-                {battle.card1}
-                <hr />
-                <p class='battle-card-attack'>
-                    Attack: {battle.card1_attack}
-                </p>
-                <br></br>
-                <p class='battle-card-health'>
-                    Health: {battle.card1_health}
-                </p>
-            </td>
-            <td>
-                {battle.card2}
-                <hr />
-                <p class='battle-card-attack'>
-                    Attack: {battle.card2_attack}
-                </p>
-                <br></br>
-                <p class='battle-card-health'>
-                    Health: {battle.card2_health}
-                </p>
-            </td>
-            <td class='battle-card-winner'>
-                <b>{battle.winner}</b>
-            </td>
-            <td>
-                <input class="delete-button" type="button" value="Delete" onClick={props.onClick} />
-            </td>
-        </tr>
-    );
-}
+function Profile() {
+    const [user, setUser] = useState([]);
 
-function HistoryTable() {
-    const [battles, setBattles] = useState([]);
-    function getBattles() {
-        fetch('/battles').
+    function getUser() {
+        fetch('/mainUser').
             then((response) => response.json()).
-            then((data) => setBattles(data));
+            then((data) => setUser(data));
     }
 
     useEffect(() => {
-        getBattles();
+        getUser();
     }, []);
 
-    function deleteAll() {
-        fetch('/deleteAll', {
-            method: 'POST'
-        }).then((response) => (response.json()))
-            .then((data) => { setBattles(data); });
-    }
 
-    function deleteBattle(id) {
-        var battle = { "id": id };
-        fetch('/delete', {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify(battle),
-        }).then((response) => (response.json()))
-            .then((data) => { setBattles(data); });
-    }
-
-    function row(battle) {
-        return <HistoryRow value={battle} onClick={() => deleteBattle(battle.id)} />
-    }
-
-    function historyRows() {
-        var rows = [];
-        var i = 0;
-        for (i = 0; i < battles.length; i++) {
-            var currentRow = battles[i];
-            var tableRow = row(currentRow);
-            rows.push(tableRow);
-        }
-        return rows;
-    }
-    return (
-        <div>
-            <p>Battle History</p>
-            <table>
-                <tr>
-                    <th>Player Card</th>
-                    <th>Opponent Card</th>
-                    <th>Winner</th>
-                </tr>
-                {historyRows()}
-                <tr>
-                    <input class="deleteall-button" type="button" value="Delete All Entries" onClick={() => deleteAll()} />
-                </tr>
-            </table>
-        </div>
-    );
-}
-
-function Profile() {
     return (
         <div className='Profile'>
             <h1 class="profile-header">Profile</h1>
-            <HistoryTable />
+            <ProfileTemplate name={user.username} value={true} />
         </div>
     );
 }
